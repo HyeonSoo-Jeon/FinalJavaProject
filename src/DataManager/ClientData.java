90b5ce56d8724d5aa55008071dfcf389c325e753
HyeonDeul <sop369@kakao.com>
2023-06-09 18:09:42 +0900

    various examples

--- src/DataManager/ClientData.java
+++ src/DataManager/ClientData.java
@@ -5,11 +5,13 @@ import java.io.Serializable;
 
 public class ClientData implements Serializable{
     String userID;
+    String nickname;
     String password;
     ArrayList<String> posts;
 
-    public ClientData(String userID, String password){
+    public ClientData(String userID, String nickname, String password){
         this.userID = userID;
+        this.nickname = nickname;
         this.password = password;
         posts = new ArrayList<>();
     }
--- src/PostUI/CreateAccount.java
+++ src/PostUI/CreateAccount.java
@@ -1,14 +1,18 @@
 package PostUI;
 
-import DataManager.ClientData;
+import DataManager.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class CreateAccount extends JPanel {
-    JTextField newID;
-    JPasswordField newPW;
-    public JButton cancelButton, registerButton;
+    JLabel confirmLabel;
+    JTextField newID, newNickname;
+    JPasswordField newPW, confirmPW;
+    public JButton cancelButton, registerButton, confirmButton;
     public CreateAccount(){
         BorderLayout borderLayout = new BorderLayout();
         setLayout(borderLayout);
@@ -20,14 +24,43 @@ public class CreateAccount extends JPanel {
 
         // ID and password input panel
         JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridLayout(4,1));
-        //inputPanel.setLayout(new GridLayout(2, 2));
+        inputPanel.setLayout(new FlowLayout());
+
         inputPanel.add(new JLabel("ID"));
         newID = new JTextField(10);
         inputPanel.add(newID);
+
+        inputPanel.add(new JLabel("Nickname"));
+        newNickname = new JTextField(10);
+        inputPanel.add(newNickname);
+
         inputPanel.add(new JLabel("Password"));
         newPW = new JPasswordField(10);
         inputPanel.add(newPW);
+
+        inputPanel.add(new JLabel("Confirm Password"));
+        confirmPW = new JPasswordField(10);
+        inputPanel.add(confirmPW);
+
+        confirmButton = new JButton("Confirm");
+        confirmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pw1 = new String(newPW.getPassword());
+                String pw2 = new String(confirmPW.getPassword());
+                if(pw1.equals(pw2)){
+                   confirmLabel.setText("Success");
+                }
+                else {
+                    confirmLabel.setText("The password is not the same.");
+                }
+            }
+        });
+        inputPanel.add(confirmButton);
+
+        confirmLabel = new JLabel("");
+        inputPanel.add(confirmLabel);
+
 
         add(inputPanel, BorderLayout.CENTER);
 
@@ -41,15 +74,15 @@ public class CreateAccount extends JPanel {
         add(buttonsPanel, BorderLayout.SOUTH);
     }
 
-    public String getID(){
-        return newID.getText();
-    }
-    public String getPW(){
-        char[] pw = newPW.getPassword();
-        return new String(pw);
-    }
-
     public void saveNewAccount(){
-        ClientData newClient = new ClientData(getID(),getPW());
+        String ID = newID.getText();
+        String nickname = newNickname.getText();
+        String PW = new String(newPW.getPassword());
+
+        ClientData newClient = new ClientData(ID,nickname,PW);
+
+        ArrayList<ClientData> clientData = ClientDataManager.loadClientData();
+        clientData.add(newClient);
+        ClientDataManager.saveClientData(clientData);
     }
 }
